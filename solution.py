ac72a707d19ecf05544c04d5b7da73a7a0184620
niko2205 <87126305+niko2205@users.noreply.github.com>
2023-04-12 23:51:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,11 +6,11 @@ from scipy.stats import ttest_1samp
 chat_id = 252623629
 
 def solution(data: pd.Series) -> bool:
-alpha = 0.1
-sample_mean = data.mean()
-n = len(data)
-sample_std = data.std(ddof=1)
-se = sample_std / np.sqrt(n)
-t_stat = (sample_mean - 300) / se
-p_value = ttest_1samp(data, 300)[1] / 2
-return p_value < alpha and t_stat < 0
+  alpha = 0.1
+  sample_mean = data.mean()
+  n = len(data)
+  sample_std = data.std(ddof=1)
+  se = sample_std / np.sqrt(n)
+  t_stat = (sample_mean - 300) / se
+  p_value = ttest_1samp(data, 300)[1] / 2
+  return p_value < alpha and t_stat < 0
