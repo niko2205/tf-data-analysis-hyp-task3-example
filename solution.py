f6d7caa061e9b44533108e17fa8dfcfdb320d031
niko2205 <87126305+niko2205@users.noreply.github.com>
2023-04-12 23:26:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 252623629
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(data: pd.Series) -> bool:
+alpha = 0.1
+sample_mean = data.mean()
+n = len(data)
+sample_std = data.std(ddof=1)
+se = sample_std / np.sqrt(n)
+t_stat = (sample_mean - 300) / se
+p_value = ttest_1samp(data, 300)[1] / 2
+return p_value < alpha and t_stat < 0
